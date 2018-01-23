fa832897424652561a17ac5bd1c61657741db846
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-23 09:24:47 +0530

    xcode

---
+++ Seven Startup Summit 2017Tests/Seven_Startup_Summit_2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Seven_Startup_Summit_2017Tests.swift
+//  Seven Startup Summit 2017Tests
+//
+//  Created by Priyadharshan Saba on 23/01/18.
+//  Copyright © 2018 Priyadharshan Saba. All rights reserved.
+//
+
+import XCTest
+@testable import Seven_Startup_Summit_2017
+
+class Seven_Startup_Summit_2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
