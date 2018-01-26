6b3154dcd34a0331466d8896debec84126defe24
PriyadharshanSaba <priyadharshan.97@gmail.com>
2018-01-27 00:17:32 +0530

    JSON API

--- Seven Startup Summit 2017/ViewController.swift
+++ Seven Startup Summit 2017/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //backgroundImage
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "win.jpg")   //SET YOUR BACKGROUND IMAGE HERE, FROM ASSETS
+        backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+
+        //mySQL-API-connection
+        let url = URL(string: " <url> ")    //API URL
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("Error")
+            }
+            else {
+                if let mydata = data {
+                    do {
+                        let myJson = try JSONSerialization.jsonObject(with: mydata, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+
+                        print(myJson[" <field> "]! ?? "noData")
+                    }
+                    catch {
+                        // catch error
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
